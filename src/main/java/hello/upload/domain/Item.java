54ba4d9525caf3c71df664409c61628350331a3c
nickhealthy <alskadmlcraz1@gmail.com>
2024-03-14 16:24:07 +0900

    섹션 11 | 예제로 구현하는 파일 업로드, 다운로드

---
+++ src/main/java/hello/upload/domain/Item.java
@@ -0,0 +1,16 @@
+package hello.upload.domain;
+
+import lombok.Data;
+
+import java.util.List;
+
+/* 상품 도메인 */
+@Data
+public class Item {
+
+    private Long id;
+    private String itemName;
+    private UploadFile attachFile;
+    private List<UploadFile> imageFiles;
+
+}
---
+++ src/main/java/hello/upload/domain/ItemRepository.java
@@ -0,0 +1,24 @@
+package hello.upload.domain;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* 상품 리포지토리 */
+@Repository
+public class ItemRepository {
+
+    private final Map<Long, Item> store = new HashMap<>();
+    private long sequence = 0L;
+
+    public Item save(Item item) {
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+    public Item findById(Long id) {
+        return store.get(id);
+    }
+}
